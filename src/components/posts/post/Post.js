e74b490a8c93b823d7de781b474e755fcd200298
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-07 14:16:34 +0100

    react

--- src/components/posts/post/Post.js
+++ src/components/posts/post/Post.js
@@ -2,10 +2,25 @@ import "./Post.css"
 import React from 'react'
 
 const Post = ({post}) => {
-    
+    const imgPostContainer={
+        height:'20vh',
+        backgroundImage:`linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.2)),url(${post.image})`,
+        backgroundRepeat:'no-repeat',
+        backgroundSize:'cover',
+        postion:'relative'
+    }
 
     return (
-        <div>
+        <div className="postContainer">
+            <div className="postHeader">
+            <img src={post.owner.picture} alt="postdisplay"/>
+                <span className="postUsername">{post.owner.lastName}</span>
+            </div>
+            <div style={imgPostContainer}>
+                <div className="postCalender">
+
+                </div>
+            </div>
             
         </div>
     )
--- src/views/posts/Posts.js
+++ src/views/posts/Posts.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import "./Posts.css"
+import Post from "../../components/posts/post/Post"
 
 const Posts = () => {
     const[posts,setPosts]=useState([])
     useEffect(()=>{
         getPosts()
     },[])
-    const BASE_URL='https://dummyapi.io/data/api/user?limit=10';
-    const APP_ID='';
+    const BASE_URL='https://dummyapi.io/data/api/post?limit=10';
+    const APP_ID='60be1637031284be4523f7f1';
     const getPosts =  async () => {
         axios.get(`${BASE_URL}/user`,{ headers: {  'app-id':  APP_ID } })
         .then(({data}) => {
