8b620b85d800deda6f572cd38c9703a076c7c422
Onabanjo Titus <titus.onabanjo@arikair.com>
2021-06-09 14:02:45 +0100

    react

---
+++ src/callback.js
@@ -0,0 +1,31 @@
+const ProcessVisa=(isWanted,userName,callBackA,callBackB,isFirstTime)=>{
+   
+    if(isWanted){
+        callbackA(userName)
+    }
+    else{
+        callBackB(userName)
+    }
+    console.log("Finished")
+}
+
+const wantedTrue=(name)=>{
+    console.log(`Call the police for :${name}`)
+}
+const wantedFalse=(name)=>{
+    console.log(`Grant visa to :${name}`)
+}
+
+const checkTravelHistory=(isFirstTime)=>{
+        if(isFirstTime){
+            console.log("Ask if excited abt first time")
+        }else{
+            console.log("How was previous trip")
+        }
+}
+
+let userName="Titus"
+let isWanted=false
+let isFirstTime=false
+setTimeout(()=>{ProcessVisa(isWanted,userName,wantedTrue,wantedFalse,isFirstTime)},3000)
+checkTravelHistory(isFirstTime)
